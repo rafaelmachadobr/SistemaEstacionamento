8979047b60527786381cc79de5443a15ff46dcfa
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-12-06 16:47:49 -0300

    feat: add parking system functionality

--- Program.cs
+++ Program.cs
@@ -1,3 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text;
+using SistemaEstacionamento.Models;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        decimal precoInicial = 0;
+        decimal precoPorHora = 0;
+
+        Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                          "Digite o preço inicial:");
+        precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Agora digite o preço por hora:");
+        precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+        Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+        bool exibirMenu = true;
+
+        while (exibirMenu)
+        {
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1. Cadastrar veículo");
+            Console.WriteLine("2. Remover veículo");
+            Console.WriteLine("3. Listar veículos");
+            Console.WriteLine("4. Encerrar");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.Write("Digite a placa do veículo: ");
+                    string placa = Console.ReadLine();
+                    estacionamento.AdicionarVeiculo(placa);
+                    break;
+                case "2":
+                    Console.Write("Digite a placa do veículo a ser removido: ");
+                    string placaRemover = Console.ReadLine();
+                    estacionamento.RemoverVeiculo(placaRemover);
+                    break;
+                case "3":
+                    estacionamento.ListarVeiculos();
+                    break;
+                case "4":
+                    Console.WriteLine("Encerrando o programa. Até mais!");
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
